697f9a119b6e1ee32ac41fd98a9121d5094bee9a
Zamanat-Rizvi <jurratrizvi786@gmail.com>
2025-11-28 20:38:22 +0530

    Made a simple madlib game!

---
+++ madlib.py
@@ -0,0 +1,5 @@
+in1 = input("Enter a noun ")
+in2 = input("Enter an adjective ")
+in3 = input("Enter a verb ")
+
+print(f"Once upon a time , A good looking {in2} was {in2} beacuse he didnt want to have {in3} ")
